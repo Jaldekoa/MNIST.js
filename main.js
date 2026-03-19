27866747adcf5d115d3f911e5f6b924a65fcab22
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-20 00:01:44 +0100

    Add drawi functionality

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
 const $canvas = document.querySelector('canvas');
 const ctx = $canvas.getContext('2d');
+
+ctx.lineWidth = 10;
+ctx.strokeStyle = "black";
+
+let isDrawing = false;
+let [lastX, lastY] = [0, 0];
+let [startX, startY] = [0, 0];
+
+$canvas.addEventListener('mousedown', startDrawing);
+$canvas.addEventListener('mousemove', draw);
+$canvas.addEventListener('mouseup', stopDrawing);
+$canvas.addEventListener('mouseleave', stopDrawing);
+
+function startDrawing(event) {
+    const { offsetX, offsetY } = event;
+
+    isDrawing = true;
+    [startX, startY] = [offsetX, offsetY];
+    [lastX, lastY] = [offsetX, offsetY];
+};
+
+function draw(event) {
+    if (!isDrawing) return;
+    const { offsetX, offsetY } = event;
+
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(offsetX, offsetY);
+    ctx.stroke();
+
+    [lastX, lastY] = [offsetX, offsetY];
+};
+
+function stopDrawing(event) {
+    isDrawing = false;
+};
