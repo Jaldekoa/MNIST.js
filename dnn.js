5ef7dd033f771a04dcbc84b235536e77680ca73c
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-21 20:31:03 +0100

    Trying DNN

--- dnn.js
+++ dnn.js
@@ -1,26 +1,39 @@
-class NeuralLayer {
-    constructor(nCon, nNeur, actFn) {
-        this.actFn = actFn;
-        this.b = nj.random([1, nNeur]);
-        this.w = nj.random([nNeur, nCon]);
+class Layer {
+    constructor() {
+        this.input = null;
+        this.output = null;
     };
 
-    sigmoid(x) {
-        return nj.sigmoid(x);
-    };
-
-    dSigmoid(x) {
-        const one = nj.ones(x.shape[0]);
-        return x.multiply(one.substract(x));
-    };
-
-    relu(x) {
-        return nj.leakyRelu(x, 0);
-    };
-
-    dRelu(x) {
-        return nj.leakyRelu(x, 0).divide(x);
-    }
+    forward(input) { };
+    backwards(outputGradient, learningRate) { };
 };
 
-export { NeuralLayer };
+class Dense extends Layer {
+    constructor(inputSize, outputSize) {
+        this.weights = nj.random([outputSize, inputSize]);
+        this.bias = nj.random([outputSize, 1]);
+    };
+
+    forward(input) {
+        this.input = input;
+        return nj.dot(this.weights, this.input).add(this.bias);
+    };
+
+    backwards(outputGradient, learningRate) {
+        const weightsGradient = nj.dot(outputGradient, this.input.T);
+        const inputGradient = nj.dot(this.weights.T, outputGradient);
+
+        this.weights = this.weights.substract(learningRate * weightsGradient);
+        this.bias = this.bias.substract(learningRate * outputGradient);
+        return inputGradient;
+    };
+};
+
+class NeuralNetwork {
+    constructor(layers) {
+        this.layers = layers;
+    }
+
+    train() { };
+    predict() { };
+};
